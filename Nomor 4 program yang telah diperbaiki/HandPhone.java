817e251da007f3c9a46e3b2759f465a4567da0c1
Ajeng Miranti <114462198+ajengmiranti@users.noreply.github.com>
2024-03-02 21:41:45 +0700

    Add files via upload

---
+++ Nomor 4 program yang telah diperbaiki/HandPhone.java
@@ -0,0 +1,32 @@
+package pbosesi1;
+
+/**
+ *
+ * @author Ajeng Miranti
+ */
+public class HandPhone {
+
+    String jenis_hp;
+    int tahun_pembuatan;
+
+    public void setDataHP(String jenis_hp, int tahun_pembuatan) {
+        this.jenis_hp = jenis_hp;
+        this.tahun_pembuatan = tahun_pembuatan;
+    }
+
+    public String getJenisHP() {
+        return jenis_hp;
+    }
+
+    public int getTahunPembuatan() {
+        return tahun_pembuatan;
+    }
+
+
+    public static void main(String[] args) {
+        HandPhone hp = new HandPhone();
+        hp.setDataHP("Samsung", 2023);
+        System.out.println("Jenis HP nya adalah : " + hp.getJenisHP());
+        System.out.println("Dibuat tahun : " + hp.getTahunPembuatan());
+    }
+}
